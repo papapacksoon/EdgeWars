8ef2d239698ad12c219e1bcfd985e08ca0b52817
Pavel Perevozchikov <11ppa@list.ru>
2019-10-24 14:48:04 +0700

    Firebase Auth First Commit

--- EdgeWars/Assets/Scripts/GameManager.cs
+++ EdgeWars/Assets/Scripts/GameManager.cs
@@ -7,19 +7,65 @@ using UnityEngine.UI;
 using System;
 using Firebase;
 using Firebase.Analytics;
+using Firebase.Auth;
+using UnityEngine.Events;
 
 public class GameManager : MonoBehaviourPunCallbacks
 {
 
     private static GameManager instance;
 
-    private void Awake()
+    
+    public UnityEvent OnFirebaseInitialized = new UnityEvent();
+
+    private FirebaseAuth _auth;
+
+    public FirebaseAuth Auth
+    {
+        get
+        {
+            if (_auth == null)
+            {
+                _auth = FirebaseAuth.GetAuth(App);
+            }
+
+            return _auth;
+        }
+    }
+
+    private FirebaseApp _app;
+
+    public FirebaseApp App
+    {
+        get
+        {
+            if (_app == null)
+            {
+                _app = GetAppSynchronous();
+            }
+
+            return _app;
+        }
+    }
+
+    private async void Awake()
     {
         DontDestroyOnLoad(this.gameObject);
 
         if (instance == null)
         {
             instance = this;
+        }
+
+        var dependencyResult = await FirebaseApp.CheckAndFixDependenciesAsync();
+        if (dependencyResult == DependencyStatus.Available)
+        {
+            _app = FirebaseApp.DefaultInstance;
+            OnFirebaseInitialized.Invoke();
+        }
+        else
+        {
+            Debug.Log(" Fialed to initalize Firebase with " + dependencyResult);
         }
 
     }
